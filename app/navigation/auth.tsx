09398d1e64cd351cadbcbc6cb0ddd441b3e265f4
Jakub Syrek <jksyrek@gmail.com>
2018-08-13 22:27:05 +0200

    Adjust react import

--- app/navigation/auth.tsx
+++ app/navigation/auth.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react'';
 import { createStackNavigator } from 'react-navigation';
 import Login from '../screens/login/Login';
 import Intro1 from '../screens/login/Intro';
--- app/navigation/navigation.tsx
+++ app/navigation/navigation.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react'';
 import { createSwitchNavigator, createStackNavigator } from 'react-navigation';
 import Login from '../screens/login/Login';
 import Landing from '../screens/landing/Landing';
