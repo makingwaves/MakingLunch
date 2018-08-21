53a461652ecd711a72b263b18220d93cf3dccbd8
Jakub Syrek <jksyrek@gmail.com>
2018-08-21 08:56:44 +0200

    Part 2

--- app/navigation/app.tsx
+++ app/navigation/app.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createStackNavigator } from 'react-navigation';
-import Main from '../screens/Main2';
+import Main from '../screens/Main';
 
 const AppNavigator = createStackNavigator({
     Main: { screen: Main }
