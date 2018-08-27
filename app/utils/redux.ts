0b13b4cdf4e1029e5e4feab9721e2dc105cd34dd
Antoni Mleczko <antos.mleczko@gmail.com>
2018-08-27 09:51:26 +0200

    Installed redux package, fixed problems with types.

--- app/utils/redux.ts
+++ app/utils/redux.ts
@@ -1,13 +1,13 @@
 import {Action, ActionCreatorsMapObject} from 'redux';
 
-export interface ActionWithPayload<P> extends Action {
+export interface ActionWithPayload<T, P> extends Action<T> {
     payload: P;
 }
 
 export type ActionUnion<T extends ActionCreatorsMapObject> = ReturnType<T[keyof T]>;
 
-export function makeAction<T extends string>(type: T): Action;
-export function makeAction<T extends string, P>(type: T, payload: P): ActionWithPayload<P>;
+export function makeAction<T extends string>(type: T): Action<T>;
+export function makeAction<T extends string, P>(type: T, payload: P): ActionWithPayload<T, P>;
 export function makeAction<T extends string, P>(type: T, payload?: P) {
     return payload ? {type, payload} : {type};
 }
