87e5e3933ea64a535462ff36d5d4672cb900823a
zgrybus <krzysztofnofz@gmail.com>
2019-01-07 09:17:33 +0100

    resposnsive height of button, backbutton

--- app/components/BackButton/style.tsx
+++ app/components/BackButton/style.tsx
@@ -1,8 +1,9 @@
 import { StyleSheet } from 'react-native';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 export default StyleSheet.create({
     container: {
-        height: 60,
+        height: hp('10%'),
         alignItems: 'flex-start',
         justifyContent: 'center',
         paddingHorizontal: 20
