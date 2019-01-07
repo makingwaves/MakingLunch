0596a626a5337d945fb143794e0f978097fabff8
zgrybus <krzysztofnofz@gmail.com>
2019-01-07 11:26:06 +0100

    KeyboardAnimationView - working with every given styles

--- app/components/KeyboardAnimationView/KeyboardAnimationView.tsx
+++ app/components/KeyboardAnimationView/KeyboardAnimationView.tsx
@@ -1,21 +1,11 @@
 import React, { Component, ReactElement } from 'react';
-import { Keyboard, EmitterSubscription, Animated, StyleProp, ViewStyle, Text, RegisteredStyle } from 'react-native';
+import { Keyboard, EmitterSubscription, Animated, StyleProp, ViewStyle, Text, RegisteredStyle, StyleSheet } from 'react-native';
 
 export interface KeyboardAnimationViewProps {
-    keyboardHideStyles: {
-        height: any;
-        opacity: any;
-    };
-    keyboardShowStyles: {
-        height: any;
-        opacity: any;
-    };
+    duration: number;
+    keyboardHideStyles: StyleProp<ViewStyle>;
+    keyboardShowStyles: StyleProp<ViewStyle>;
     animationViewStyles?: StyleProp<ViewStyle>;
-};
-
-export interface KeyboardAnimationViewState {
-    height: Animated.Value;
-    opacity: Animated.Value;
 };
 
 export enum Listeners {
@@ -25,18 +15,17 @@ export enum Listeners {
 
 export type ListenersObjectType = { [key in Listeners]: EmitterSubscription };
 
-class KeyboardAnimationView extends Component<KeyboardAnimationViewProps, KeyboardAnimationViewState> {
-    public state: KeyboardAnimationViewState;
-
+class KeyboardAnimationView extends Component<KeyboardAnimationViewProps> {
     private listenersObject: ListenersObjectType;
+
+    private animatedKeysStyles: string[];
+    private animatedStylesValues: { [key: string]: Animated.Value };
 
     constructor(props: KeyboardAnimationViewProps) {
         super(props);
-
-        this.state = {
-            height: new Animated.Value(this.props.keyboardHideStyles.height),
-            opacity: new Animated.Value(this.props.keyboardHideStyles.opacity)
-        };
+        
+        this.animatedKeysStyles = this.getAnimatedKeysStyles(this.props.keyboardShowStyles, this.props.keyboardHideStyles);
+        this.animatedStylesValues = this.getAnimatedObjectValues(this.animatedKeysStyles, this.props.keyboardHideStyles);
     }
 
     public componentDidMount(): void {
@@ -48,6 +37,25 @@ class KeyboardAnimationView extends Component<KeyboardAnimationViewProps, Keyboa
             .map(key => this.listenersObject[key].remove());
     }
 
+    private getAnimatedObjectValues(animatedKeys: string[], initialStyles: StyleProp<ViewStyle>): { [key: string]: Animated.Value } {
+        return animatedKeys && animatedKeys
+            .reduce((animatedObject, key) => (animatedObject[key] = new Animated.Value(initialStyles[key]),animatedObject ), {});
+    }
+
+    private getAnimatedKeysStyles(keyboardShowStyles: StyleProp<ViewStyle>, keyboardHideStyles: StyleProp<ViewStyle>): string[] {
+        const [showKeys, hideKeys] = [
+            Object.keys(keyboardShowStyles),
+            Object.keys(keyboardHideStyles)
+        ];
+        if(!this.areKeysTheSame(showKeys, hideKeys))
+            throw new Error('KeyboardAnimationView - Given props object keys are not the same. Fix it.');
+        return showKeys;
+    }
+
+    private areKeysTheSame(firstArray: string[], secondArray: string[]): boolean {
+        return firstArray.length === secondArray.length && !firstArray.some(showKey => !secondArray.includes(showKey));
+    }
+
     private getListeners(): ListenersObjectType {
         return {
             [Listeners.didShow]: Keyboard.addListener(Listeners.didShow, this.onKeyboardShow),
@@ -55,46 +63,37 @@ class KeyboardAnimationView extends Component<KeyboardAnimationViewProps, Keyboa
         }
     }
 
+    private getAnimationArray(keys: string[], stateStylesObject: StyleProp<ViewStyle>, valuesObject: { [key: string]: Animated.Value }): Animated.CompositeAnimation[] {
+        return keys && keys
+            .map(key => Animated.timing(valuesObject[key], {
+                toValue: stateStylesObject[key],
+                duration: this.props.duration
+            }));
+    }
+
     private onKeyboardShow = () => {
-        Animated.parallel([
-            Animated.timing(this.state.opacity, {
-                toValue: this.props.keyboardShowStyles.opacity,
-                duration: 300
-            }),
-            Animated.timing(this.state.height, {
-                toValue: this.props.keyboardShowStyles.height,
-                duration: 300
-            })
-        ]).start();
+        Animated.parallel(
+            this.getAnimationArray(this.animatedKeysStyles, this.props.keyboardShowStyles, this.animatedStylesValues)
+        ).start();
     }
 
     private onKeyboardHide = () => {
-        Animated.parallel([
-            Animated.timing(this.state.opacity, {
-                toValue: this.props.keyboardHideStyles.opacity,
-                duration: 300
-            }),
-            Animated.timing(this.state.height, {
-                toValue: this.props.keyboardHideStyles.height,
-                duration: 300
-            })
-        ]).start();
+        Animated.parallel(
+            this.getAnimationArray(this.animatedKeysStyles, this.props.keyboardHideStyles, this.animatedStylesValues)
+        ).start();
     }
 
     public render(): ReactElement<Element> {
         const {
             children,
+            animationViewStyles
         } = this.props;
-        const {
-            height,
-            opacity
-        } = this.state;
 
         return (
             <Animated.View
                 style={{
-                    height,
-                    opacity
+                    animationViewStyles,
+                    ...this.animatedStylesValues
                 }}
             >
                 {children}
