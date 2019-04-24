095450e69bcc39d38bfc17eea8637d21b37fcbee
zgrybus <krzysztofnofz@gmail.com>
2019-04-24 10:28:34 +0200

    missing android configuration

--- android/app/src/main/java/com/makinglunch/MainApplication.java
+++ android/app/src/main/java/com/makinglunch/MainApplication.java
@@ -1,17 +1,36 @@
-package com.makinglunch;
+package com.makingLunch;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import io.github.traviskn.rnuuidgenerator.RNUUIDGeneratorPackage;
+import com.reactlibrary.securekeystore.RNSecureKeyStorePackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -22,7 +41,16 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNGestureHandlerPackage(),
+          new MapsPackage(),
+          new RNUUIDGeneratorPackage(),
+          new RNSecureKeyStorePackage(),
+          new ReactNativeConfigPackage(),
+          new FBSDKPackage(mCallbackManager),
+          new RNGoogleSigninPackage(),
+          new ReactNativePushNotificationPackage(),
+          new FastImageViewPackage()
       );
     }
 
@@ -40,6 +68,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
+    AppEventsLogger.activateApp(this);
     SoLoader.init(this, /* native exopackage */ false);
   }
 }
