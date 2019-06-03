308f087b0b20740967c1d79a45a1f9b78d432646
Daniel Domurad <daniel.domurad@makingwaves.com>
2019-06-03 15:11:49 +0200

    Android deployment configuration added

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -6,3 +6,20 @@ printf "%s\n" $ENV_WHITELIST
 set | egrep -e $ENV_WHITELIST | sed 's/^CONFIG_//g' > .env
 printf "\n.env created with contents:\n\n"
 cat .env
+
+printf "Create google services config file\n"
+echo "$GOOGLE_SERVICES" > ./android/app/google-services.json
+sed -i -e 's/\\"/'\"'/g' ./android/app/google-services.json
+echo "google-services file content:"
+cat ./android/app/google-services.json
+
+if [$RN_RELEASE_TYPE != "prod"]; then
+    printf "Install imagemagick:\n"
+    brew install imagemagick
+else
+    printf "Skip imagemagick installation for production relese:\n"
+fi
+
+printf "Executing fastlane prep_relese_type:\n"
+bundle install
+bundle exec fastlane prep_release_type
