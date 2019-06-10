2f3afc5c7ccbb95e6130a9d357b3f76a35da7c0e
Daniel Domurad <daniel.domurad@makingwaves.com>
2019-06-10 15:53:09 +0200

    udpate

--- app/components/AppPopup/AppPopup.tsx
+++ app/components/AppPopup/AppPopup.tsx
@@ -5,61 +5,32 @@ import styles from './style';
 import Display from '../Display';
 
 
-export interface AppPopupProps {
-    title: string;
-    iconUrl?: ImageSourcePropType;
-    showError: boolean;
-    description?: string;
-    showDuration: number;
-};
+// export interface AppPopupProps {
+//     title: string;
+//     iconUrl?: ImageSourcePropType;
+//     showError: boolean;
+//     description?: string;
+//     showDuration: number;
+// };
 
 export interface AppPopupState {
     isVisible: boolean;
 };
 
-class AppPopup extends PureComponent<AppPopupProps, AppPopupState> {
+class AppPopup extends PureComponent<object, AppPopupState> {
     public state: AppPopupState;
 
-    constructor(props: AppPopupProps) {
+    constructor(props: object) {
         super(props);
-
-        this.state = {
-            isVisible: false
-        };
-    }
-
-    public componentDidUpdate(prevProps: AppPopupProps, prevState: AppPopupState): void {
-        if (!this.state.isVisible && (prevProps !== this.props && this.props.showError))
-            this.setState(prevState => ({
-                isVisible: true
-            }), () => {
-                setTimeout(() => (
-                    this.setState(prevState => ({ isVisible: false }))
-                ), this.props.showDuration);
-            });
     }
 
     public render() {
-        const {
-            title,
-            iconUrl,
-            description,
-        } = this.props;
-        const {
-            isVisible
-        } = this.state;
 
         return (
-            <Display style={styles.container} enable={isVisible} enter={'fadeInDown'} exit={'fadeOutUp'} defaultDuration={300}>
-                {iconUrl && <Image source={iconUrl} style={styles.icon} />}
-                <View>
-                    <Text style={styles.title}>{title}</Text>
-                    {description ? <Text style={styles.description}>{description}</Text> : null}
-                </View>
-            </Display>
+            <Text>Elo</Text>
         )
     }
 }
 
-
+const mapStateToProps = (store: IAppSta)
 export default AppPopup;
