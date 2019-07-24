5e34e2971536d19d400b6504af9bc0cd0f795b38
Paweł Bród <pawel.brod@gmail.com>
2019-07-24 20:40:23 +0200

    fix profile

--- app/state/profile/selectors.ts
+++ app/state/profile/selectors.ts
@@ -1,7 +1,11 @@
 import { createSelector } from 'reselect';
 import { AppState } from '../state';
+import {RequestState} from "@app/state/common/types";
 
 const profileSelector = (state: AppState) => state.profile;
+
+export const getIfProfileLoading = (state: AppState) => state.lunches.request.state === RequestState.inProgress;
+export const getProfileError = (state: AppState) => state.lunches.request.errorMsg;
 
 export const getProfile = createSelector(
     profileSelector,
